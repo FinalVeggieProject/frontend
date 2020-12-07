22a211ab88af82099a5616adca1df82ebda32e3a
Sergio <srosduran@gmail.com>
2020-12-07 12:37:05 +0100

    creating and logging users

--- src/App.js
+++ src/App.js
@@ -1,11 +1,111 @@
 import './App.css';
 import React from 'react'
+import SignUp from './components/SignUp';
+import LogIn from './components/LogIn';
+
+import { Link, Route, Redirect } from 'react-router-dom';
+import UserService from './services/UserService';
 
 class App extends React.Component {
+
+	state = {
+		isLogged: {},
+		newUser: { username: '', password: '', email: ''},
+    loggingUser: { username: '', password: '', email: ''},
+	};
+
+  service = new UserService();
+
+  //SIGNUP CONFIG
+  submitSignUp = (event) => {
+    event.preventDefault();
+    this.service.signup(this.state.newUser.username, this.state.newUser.password, this.state.newUser.email)
+      .then((result) => {
+        console.log(result);
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+    
+  };
+
+	changeHandlerSignUp = (_eventTarget) => {
+		this.setState({ newUser: { ...this.state.newUser, [_eventTarget.name]: _eventTarget.value } });
+  };
+  
+  //LOGIN CONFIG
+	submitLogIn = (event) => {
+		event.preventDefault();
+		this.service
+			.login(this.state.loggingUser.username, this.state.loggingUser.password)
+			.then(() => {
+				this.checkIfLoggedIn()
+			})
+			.catch((err) => {
+				console.log('Sorry something went wrong on submit.', err);
+			});
+	};
+
+	changeHandlerLogIn = (_eventTarget) => {
+		this.setState({ loggingUser: { ...this.state.loggingUser, [_eventTarget.name]: _eventTarget.value } });
+	};
+
+	checkIfLoggedIn = () => {
+    this.service.loggedin()
+    .then((result)=>{
+      this.setState({isLogged: result})
+    })
+  };
+  
+  logOut = ()=>{
+    this.service.logout()
+    .then((result)=>{
+      console.log(result)
+      this.checkIfLoggedIn()
+    })
+    .catch((err)=>{
+      console.log(err)
+    })
+  }
+
+	componentDidMount() {
+		this.checkIfLoggedIn();
+	}
+
+
   render(){
     return (
       <div className="App">
-       <h1>Holi</h1>
+        <h1>LANDING PAGE</h1>
+
+        {!this.state.isLogged.username && <Link to="/signup">Sign Up</Link>}
+        <br />
+        {!this.state.isLogged.username && <Link to="/login">Log In</Link>}
+        <br />
+      
+
+        <Route
+					path="/signup"
+					render={() => (
+						!this.state.isLogged.username
+							? <SignUp submitSignUp={this.submitSignUp} newUser={this.state.newUser} changeHandlerSignUp={this.changeHandlerSignUp}/>
+							: <Redirect to='/' />
+					)}
+				/>
+
+				<Route
+					path="/login"
+					render={() => (
+						<LogIn
+							submitLogIn={this.submitLogIn}
+							loggingUser={this.state.loggingUser}
+							changeHandlerLogIn={this.changeHandlerLogIn}
+						/>
+					)}
+				/>
+
+       
+        {/* {this.state.isLogged.username && <button onClick={()=>this.logOut()}>Log Out</button>} */}
       </div>
     );
   }
---
+++ src/components/SignUp.js
@@ -0,0 +1,42 @@
+import React from 'react'
+
+const SignUp = (props)=>{
+  return(
+    <div>
+      <h2>Sign Up</h2>
+      <form onSubmit={props.submitSignUp}>
+
+      <label htmlFor="username">Username: </label>
+        <input 
+          type="text" 
+          name="username" 
+          value={props.newUser.username} 
+          onChange={(event)=>props.changeHandlerSignUp(event.target)}
+        />
+
+      <label htmlFor="email">E-mail: </label>
+        <input 
+          type="text" 
+          name="email" 
+          value={props.newUser.email} 
+          onChange={(event)=>props.changeHandlerSignUp(event.target)}
+        />
+        
+        <label htmlFor="password">Password: </label>
+        <input 
+          type="password" 
+          name="password" 
+          value={props.newUser.password} 
+          onChange={(event)=>props.changeHandlerSignUp(event.target)}
+        />
+
+        <button type="submit">Crear Usuario</button>
+
+        {/* <p>{message}</p> */}
+
+      </form>
+    </div>
+  )
+}
+
+export default SignUp
