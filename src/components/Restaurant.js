f8b94d742fdea5b39202251744ec0dd37e049abf
root <root@Pablo.localdomain>
2020-12-10 15:41:15 +0100

    restarant added

---
+++ src/components/Restaurant.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import UserService from '../services/UserService';
+import {Link} from 'react-router-dom';
+import EditInput from './EditInput';
+
+class Restaurant extends React.Component {
+
+  state = {
+
+  }
+
+
+
+  render(){
+    return(
+      <div>
+        
+      </div>
+    )
+  }
+}
