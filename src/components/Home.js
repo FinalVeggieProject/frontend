2270218682ac9a4a1ac8717d3a6ea3a29297ea98
Sergio <srosduran@gmail.com>
2020-12-15 16:39:11 +0100

    adding styles to home

--- src/components/Home.js
+++ src/components/Home.js
@@ -43,6 +43,7 @@ class Home extends React.Component {
     render(){
         return(
             <div className="home">
+            <h3>Últimas recetas</h3>
                 <AwesomeSlider className="recipesSlider">
                 {
                     this.state.globalRecipes.map((recipe)=>{
@@ -51,7 +52,7 @@ class Home extends React.Component {
                 }
                 </AwesomeSlider>
                 
-
+                <h3>Últimos restaurantes</h3>
                 <AwesomeSlider className="restaurantsSlider">
                 {
                     this.state.globalRestaurants.map((restaurant)=>{
